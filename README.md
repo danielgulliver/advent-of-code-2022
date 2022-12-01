d8c5416946476b4f4f53d396cad605da26cc507c
Daniel Gulliver <danielgulliver@protonmail.com>
2022-12-01 22:27:41 +0000

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+This repo contains my solutions for the Advent of Code coding challenge for the year 2022. The project is set up as a Node.js app written in TypeScript.
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
 import day1 from './days/day1'
 
+console.log("===================")
 console.log("Advent of Code 2022")
+console.log("===================")
 
-day1()
+const day = parseInt(process.argv[2]) ?? 0
+
+if (day > 0 && day <= 25) {
+    console.log(`Day ${day}:\n`)
+}
+
+switch (day) {
+    case 0:
+        console.log("No day specified. Please specify a day (e.g. 1): npm start -- <day>")
+        break
+    case 1:
+        day1()
+        break
+    default:
+        const message = (day <= 25) ? `The solution for Day ${day} has not been implemented yet.` : "Please specify a day between 1 and 25."
+        console.log(message)
+}
